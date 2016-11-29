2bf11c1edf897569d1ec7cef5a50bf8d284d29e5
Ken Wilcox <wilcoxk@gmail.com>
2016-11-29 14:11:06 -0700

    making progress, missing _start

--- hello.c
+++ hello.c
@@ -1,4 +1,29 @@
-#include <unistd.h>
+//#include <unistd.h>
+void* syscall5 (
+	void* number,
+	void* arg1,
+	void* arg2,
+	void* arg3,
+	void* arg4,
+	void* arg5
+);
+
+typedef unsigned long int uintptr; /* size_t */
+typedef long int intptr; /* size_t */
+
+static
+intptr write(int fd, void const* data, uintptr nbytes)
+{
+	return (intptr)
+		syscall5(
+			(void*)1, /* SYS_write */
+			(void*)(intptr)fd,
+			(void*)data,
+			(void*)nbytes,
+			0, /* ignored */
+			0  /* ignored */
+		);
+}
 
 int main(int argc, char* argv[])
 {
